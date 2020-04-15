c071312b31ff86db622964198effec89398d4497
Deekshant <dmalvi@gammastack.com>
2020-04-16 03:12:33 +0530

    added index file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.html('HELLLO WORLDD!!, Response from node server !!');
+    res.send("<html>Hello World !! This is ton site hosted from  node server<html>\n");
 })
 
 app.listen(port, () => {
