d46996510a6b1dc11f77abfdcbb689b96abc2019
Deekshant <dmalvi@gammastack.com>
2020-04-26 19:53:20 +0530

    added relative path to root

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const os =require('os')
+
+__dirname + 
 
 app.get('/', (req, res) => {
-    res.send("<html>Hello World !! This is ton site edited with ton dns hello.temp.ton hosted from  node server<html>\n");
+    res.sendFile("index.html", {root : __dirname});
+    //res.send("<html>Hello World !! This is ton site edited with ton dns hello.temp.ton hosted from  node server<html>\n");
 })
 
 app.listen(port, () => {
