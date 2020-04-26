0e95b332fa2a0b1135eba2081adc8646a830349f
Deekshant <dmalvi@gammastack.com>
2020-04-26 20:53:11 +0530

    image

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const os =require('os')
 __dirname + 
 
 app.get('/', (req, res) => {
-    res.sendFile("index.html", {root : __dirname});
+    res.sendFile("ton.jpeg", {root : __dirname});
     //res.send("<html>Hello World !! This is ton site edited with ton dns hello.temp.ton hosted from  node server<html>\n");
 })
 
